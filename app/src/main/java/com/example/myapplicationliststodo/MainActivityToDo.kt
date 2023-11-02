042cf80d1a7238a10913b3194c1aa0bc2ed880c3
alex_ <alex_182_sp@outlook.com>
2023-11-02 15:03:47 -0600

    App to remember things to do

---
+++ app/src/main/java/com/example/myapplicationliststodo/MainActivityToDo.kt
@@ -0,0 +1,11 @@
+package com.example.myapplicationliststodo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityToDo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_to_do)
+    }
+}
